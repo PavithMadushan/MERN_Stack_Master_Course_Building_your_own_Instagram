e3fa26ea6886ef1010f03d6a1bec38823b52e40e
PavithMadushan <pavithmadushan@gmail.com>
2022-12-06 22:53:23 +0530

    1 st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/Navbar";
+import "./App.css";
+import {BrowserRouter,Route} from 'react-router-dom';
+import Home from "./components/screens/Home";
+import Signup from "./components/screens/Signup";
+import Signin from "./components/screens/Login";
+import Profile from "./components/screens/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+
+    <Route exact path="/">
+      <Home/>
+    </Route>
+
+    <Route path="/signin">
+      <Signin/>
+    </Route>
+
+    <Route path="/signup">
+      <Signup/>
+    </Route>
+
+    <Route path="/profile">
+      <Profile/>
+    </Route> 
+
+{/* <Route>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<NoPage />} />
+        </Route>
+      </Route> */}
+    
+    </BrowserRouter>
   );
 }
 
